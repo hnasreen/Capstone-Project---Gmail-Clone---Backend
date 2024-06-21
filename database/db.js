3676283f6a8d681819a1f71b6f15101ac0780fc3
hnasreen <hnasreen1993@gmail.com>
2024-06-21 15:25:38 +0530

    Implementation phase 1 - Functionalities - Send Emails, Draft, Sent, Starred, Bin, All Mails

---
+++ database/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const Connection = () => {
+    try{
+
+        const MONGODB_URI = `mongodb+srv://hnasreen1993:${process.env.DB_PASSWORD}@mongodb1.mqllmd1.mongodb.net/`
+        mongoose.connect(MONGODB_URI,{useNewUrlParser:true})
+        console.log('database connected successfully')
+    }catch(error){
+        console.log('Error while connecting with the database',error.message)
+    }
+}
+
+export default Connection;
---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Connection from './database/db.js';
+import routes from './routes/route.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors())
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json({extended:true}))
+
+app.use('/',routes)
+
+const PORT = process.env.PORT || 8000;
+
+Connection();
+
+app.listen(PORT,()=> console.log(`Server has started on PORT ${PORT}`))
